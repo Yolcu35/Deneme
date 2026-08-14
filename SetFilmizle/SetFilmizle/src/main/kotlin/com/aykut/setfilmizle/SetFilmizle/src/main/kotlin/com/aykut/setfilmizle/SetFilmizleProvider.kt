6bf60efd49e7597090ca36df370d8082d719af21
Yolcu35 <katuran70@gmail.com>
2026-08-14 03:46:28 +0300

    Create SetFilmizleProvider.kt

---
+++ SetFilmizle/SetFilmizle/src/main/kotlin/com/aykut/setfilmizle/SetFilmizle/src/main/kotlin/com/aykut/setfilmizle/SetFilmizleProvider.kt
@@ -0,0 +1,25 @@
+package com.aykut.setfilmizle
+
+import com.lagradost.cloudstream3.MainAPI
+import com.lagradost.cloudstream3.SearchResponse
+import com.lagradost.cloudstream3.TvType
+
+class SetFilmizleProvider : MainAPI() {
+
+    override var mainUrl = "https://www.setfilmizle.ltd/"
+    override var name = "SetFilmizle"
+    override var lang = "tr"
+
+    override val supportedTypes = setOf(
+        TvType.Movie,
+        TvType.TvSeries
+    )
+
+    override val hasMainPage = false
+
+    override suspend fun search(
+        query: String
+    ): List<SearchResponse> {
+        return emptyList()
+    }
+}
